c6e5318e7478e4c406b1cc5546df928c3d98a96b
jiazhi.sun <jiazhi.sun@mu-c1-16.mullen.pvt.k12.co.us>
2018-09-05 14:52:57 -0600

    challenge 1, 2, 3

---
+++ Arrays challenges/src/arrays.java
@@ -0,0 +1,47 @@
+
+public class arrays
+	{
+
+		public static void main(String[] args)
+			{
+				//challenge1();
+				//challenge2();
+				//challenge3();
+				challenge4();
+				
+
+			}
+
+		private static void challenge4()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+		private static void challenge3()
+			{
+				int [] number = {1 , 2 , 3 , 4 , 5};
+				int length1 = number.length;
+				for(int i = 0; i < length1; i++ ){
+					System.out.println(number [i]);
+				}
+				
+			}
+
+		private static void challenge2()
+			{
+				
+				String [ ] fruit = {"Apple" , "Watermellon" , "Strawberry" , "Blueberry"};
+				int length1 = fruit.length;
+				System.out.println(fruit [length1 - 1]);
+				
+			}
+
+		private static void challenge1()
+			{
+				String [ ] fruit = {"Apple" , "Watermellon" , "Strawberry" , "Blueberry"};
+				System.out.println(fruit [2]);
+				
+			}
+
+	}
